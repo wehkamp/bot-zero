c69b45aeeab8130d427e94dd09cd59cd4d401004
Kees C. Bakker <c.bakker@gmx.net>
2018-05-12 13:20:19 +0200

    Added todo list as example for fluent command mapper notation.

---
+++ scripts/todo.js
@@ -0,0 +1,77 @@
+// Description:
+//  Creates a small global todo list.
+//
+// Commands:
+//  hubot todo - shows the todo list.
+//  hubot todo {description} - adds a new item to the list.
+//  hubot todo remove {description} - removed items from the list that match the description.
+//
+// Author:
+//  KeesCBakker (kbakker@wehkmap.nl)
+"strict"
+
+const { tool } = require("hubot-command-mapper");
+
+//this example show how to use the fluent notation
+//of the command mapper.
+
+module.exports = robot => {
+    var _todos = [];
+
+    tool("todo")
+
+        //Adds an item to the ToDo list
+        //hubot toto add {description}
+        //hubot todo {description}
+        .command("add")
+        .alias("")
+        .parameterForRest("item")
+        .invoke((tool, robot, res, match, values) => {
+            let item = values["item"];
+            _todos.push(item);
+            res.reply(`Added _${item}_ to the list.`)
+        })
+
+        //Lists the ToDo list
+        //hubot todo list
+        //hubot todo lst
+        //hubot todo
+        .command("list")
+        .alias("lst")
+        .alias("")
+        .invoke((tool, robot, res, match, values) => {
+            if (_todos.length === 0) {
+                res.reply("The list is empty.")
+                return;
+            }
+
+            let i = 0;
+            let str = "The following items are on the list:\n";
+            str += _todos.map(t => ++i + t).join("\n");
+            res.reply(str);
+        })
+        
+        //Removes items that match from the ToDo list
+        //hubot todo remove {description}
+        //hubot todo rm {description}
+        //hubot todo del {description}
+        .command("remove")
+        .alias("rm")
+        .alias("del")
+        .parameterForRest("item")
+        .invoke((tool, robot, res, match, values) => {
+            let item = values["item"].toLowerCase();
+            let l = _todos.length;
+            _todos = _todos.filter(f => f.toLowerCase().indexOf(item) == -1);
+            i = l - _todos.length;
+            if (i === 1) {
+                res.reply("1 item was removed.")
+            }
+            else {
+                res.reply(`${i} items were removed.`);
+            }
+        })
+        
+        //maps everything together:
+        .map(robot);
+}
---
+++ tests/todo.spec.js
@@ -0,0 +1,59 @@
+"strict"
+
+//don't let the Hubot Command Mapper echo through the
+//test results - set this before loading any tool.
+process.env.HCM_VERBOSE = "false";
+
+const pretend = require("hubot-pretend");
+const { expect } = require("chai");
+const todo = require("./../scripts/todo");
+
+require("mocha")
+
+describe("Norris tool", () => {
+
+    //initializes a new version of the 
+    //robot before each test (the it's)
+    beforeEach(() => {
+        pretend.name = "hubot";
+        pretend.alias = "hubot";
+        pretend.start();
+        todo(pretend.robot);
+    });
+
+    //shut the bot down after each test
+    afterEach(() => pretend.shutdown());
+
+    it("Scenario: add, add, add, list, remove, list", done => {
+
+        let user = pretend.user("kees");
+
+        user
+            .send("@hubot todo Boter halen")
+            .then(x => user.send("@hubot todo Kaas halen"))
+            .then(x => user.send("@hubot todo Eieren halen"))
+            .then(x => user.send("@hubot todo"))
+            .then(x => user.send("@hubot todo remove er"))
+            .then(x => user.send("@hubot todo list"))
+            .then(x => {
+                expect(pretend.messages).to.eql(
+                    [
+                        ['kees', '@hubot todo Boter halen'],
+                        ['hubot', '@kees Added the item to the list.'],
+                        ['kees', '@hubot todo Kaas halen'],
+                        ['hubot', '@kees Added the item to the list.'],
+                        ['kees', '@hubot todo Eieren halen'],
+                        ['hubot', '@kees Added the item to the list.'],
+                        ['kees', '@hubot todo'],
+                        ['hubot', '@kees The following items are on the list:\n1Boter halen\n2Kaas halen\n3Eieren halen'],
+                        ['kees', '@hubot todo remove er'],
+                        ['hubot', '@kees 2 items were removed.'],
+                        ['kees', '@hubot todo list'],
+                        ['hubot', "@kees The following items are on the list:\n1Kaas halen"]
+                    ]
+                );
+            })
+            .then(x => done())
+            .catch(ex => done(ex));
+    });
+});
