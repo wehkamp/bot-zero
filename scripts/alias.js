98166f615210d4a2a01ded77afba89213bc69843
Kees C. Bakker <c.bakker@gmx.net>
2018-09-29 18:30:10 +0200

    Added alias implementation. Not testable though :-(.

---
+++ scripts/alias.js
@@ -0,0 +1,11 @@
+const { alias } = require('hubot-command-mapper')
+
+/* aliasses can be added to this table. It proveds an
+ * easy way to map complex commands with lots of parameters
+ * into simple versions.
+ */
+const table = {
+  'fav-norris': 'norris nr 112'
+}
+
+module.exports = robot => alias(robot, table)
