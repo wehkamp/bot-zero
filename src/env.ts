a0ca7df60bd3b02a564378c87ddf9bc2e7d6b8ea
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2021-11-29 10:13:16 +0100

    Fix error with no token present.

--- src/env.ts
+++ src/env.ts
@@ -36,7 +36,9 @@ export function validateToken(config: string[]) {
       .map(x => x.split("="))
       .filter(x => x[0] == "HUBOT_SLACK_TOKEN")
       .map(x => x[1].trim())
-      .find(Boolean) || process.env.HUBOT_SLACK_TOKEN
+      .find(Boolean) ||
+    process.env.HUBOT_SLACK_TOKEN ||
+    ""
 
   if (token.startsWith('"') && token.endsWith('"')) {
     token = token.substr(1, token.length - 2)
