e6a764d580501c91e3a5baaa41672af3dd8aadf0
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2023-06-07 08:24:03 +0000

    Typo

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -6,7 +6,7 @@
 git config --global --add safe.directory "$PWD"
 
 # config local GPG for signing
-# files:
+# fixes:
 # - error: cannot run C:\Program Files (x86)\Gpg4win..\GnuPG\bin\gpg.exe: No such file or directory.
 git config --global gpg.program gpg 
 
