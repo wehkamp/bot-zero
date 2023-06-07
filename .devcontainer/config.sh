1e9406159811dcf17f0d9bd6e8b4f256499b390a
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2023-06-07 07:25:07 +0000

    Add more comments

--- .devcontainer/config.sh
+++ .devcontainer/config.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 # trust the repo
+# fixes:
+# - fatal: detected dubious ownership in repository at '/workspaces/bot-zero'.
 git config --global --add safe.directory "$PWD"
 
 # config local GPG for signing
+# files:
+# - error: cannot run C:\Program Files (x86)\Gpg4win..\GnuPG\bin\gpg.exe: No such file or directory.
 git config --global gpg.program gpg 
 
 # install NPM packages
